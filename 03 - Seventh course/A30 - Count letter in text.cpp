fbdb89b8dc59a041f010613e563468ea04849419
Youssef645561 <kdsnsnsh@gmail.com>
2025-05-15 19:30:51 +0300

    Update and rename A30.cpp to A30 - Count letter in text.cpp

---
+++ 03 - Seventh course/A30 - Count letter in text.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+string ReadText()
+{
+	string Text = "";
+
+	cout << "Enter a text :\n";
+	getline(cin >> ws, Text);
+
+
+	return Text;
+}
+
+char ReadChar()
+{
+	char Letter;
+
+	cout << "\nEnter a letter :\n";
+	cin >> Letter;
+
+	return Letter;
+}
+
+short CountLetterRepition(string Text, char LetterToCount)
+{
+	short Counter = 0;
+
+	for (short i = 0;i < Text.length();i++)
+	{
+		if (Text[i] == LetterToCount)
+		{
+			Counter++;
+		}
+	}
+
+	return Counter;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	string Text = ReadText();
+	char LetterToCount = ReadChar();
+
+
+	cout << "\nLetter \'" << LetterToCount << "\' counter = " << CountLetterRepition(Text, LetterToCount) << endl;
+}
