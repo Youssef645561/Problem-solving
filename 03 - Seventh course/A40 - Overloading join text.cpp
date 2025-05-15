0ab258187a0abb059ddc1b29ab22fec011f3ae85
Youssef645561 <kdsnsnsh@gmail.com>
2025-05-16 02:09:48 +0300

    Update and rename A40.cpp to A40 - Overloading join text.cpp

---
+++ 03 - Seventh course/A40 - Overloading join text.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+string ReadText()
+{
+	string Text = "";
+
+	cout << "Enter a text :\n";
+	getline(cin >> ws, Text);
+
+
+	return Text;
+}
+
+vector <string> MySplitText(string Text, string Delim = " ")
+{
+	vector <string> vWords;
+
+	short Pos = 0;
+	string Word = "";
+
+	while ((Pos = Text.find(Delim)) != std::string::npos)
+	{
+		Word = Text.substr(0, Pos);
+
+		if (!Word.empty())
+		{
+			vWords.push_back(Word);
+		}
+
+		Text.erase(0, (Pos + Delim.length()));
+	}
+
+	if (!Text.empty())
+	{
+		vWords.push_back(Text);
+	}
+
+	return vWords;
+}
+
+string MyJoinText(vector <string> vWords, string Delim = " ")
+{
+	string Text;
+
+	for (string& Element : vWords)
+	{
+		Text += Element + Delim;
+	}
+
+	return Text.substr(0, (Text.length() - Delim.length()));
+}
+
+string MyJoinText(string ArrWords[], short ArrLength, string Delim = " ")
+{
+	string Text;
+
+	for (short i = 0; i < ArrLength; i++)
+	{
+		Text += ArrWords[i] + Delim;
+	}
+
+	return Text.substr(0, (Text.length() - Delim.length()));
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	string Text = ReadText();
+
+	vector <string> vWords = MySplitText(Text);
+
+	string ArrWords[] = { "Hello", "I'm", "Youssef" };
+
+	cout << "\n\nVector after join : ";
+	cout << MyJoinText(vWords, "; ") << endl;
+
+
+	cout << "\nArray after join : ";
+	cout << MyJoinText(ArrWords, 3, "; ") << endl;
+}
