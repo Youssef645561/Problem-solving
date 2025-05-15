db68a5b32a107019c3b0ca5389db1684b4072c36
Youssef645561 <kdsnsnsh@gmail.com>
2025-05-15 15:19:42 +0300

    Update and rename A16.cpp to A16 - Is matrix sparse.cpp

---
+++ 03 - Seventh course/A16 - Is matrix sparse.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+
+using namespace std;
+
+int RandomNumInRange(int From, int To)
+{
+	return rand() % (To - From + 1) + From;
+}
+
+void FillMatrixWithRandomNums(int Matrix[3][3], const short& Rows, const short& Cols)
+{
+	for (short i = 0; i < Rows; i++)
+	{
+		for (short C = 0; C < Cols; C++)
+		{
+			Matrix[i][C] = RandomNumInRange(1, 10);
+		}
+	}
+}
+
+void PrintMatrix(int Matrix[3][3], short Rows, short Cols)
+{
+	for (short i = 0; i < Rows; i++)
+	{
+		for (short C = 0; C < Cols; C++)
+		{
+			printf("%02d\t", Matrix[i][C]);
+		}
+		printf("\n");
+	}
+
+	cout << endl;
+}
+
+short CountNumInMatrix(int Matrix[3][3], short Rows, short Cols, int NumToCount)
+{
+	short Counter = 0;
+
+	for (short i = 0; i < Rows; i++)
+	{
+		for (short C = 0; C < Cols; C++)
+		{
+			if (Matrix[i][C] == NumToCount)
+			{
+				Counter++;
+			}
+		}
+	}
+
+	return Counter;
+}
+
+short IsMatrixSparse(int Matrix[3][3], short Rows, short Cols)
+{
+	return (CountNumInMatrix(Matrix, Rows, Cols, 0) > ((Rows * Cols) / 2));
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int Matrix[3][3];
+
+	FillMatrixWithRandomNums(Matrix, 3, 3);
+	cout << "Matrix :\n";
+	PrintMatrix(Matrix, 3, 3);
+
+	if (IsMatrixSparse(Matrix, 3, 3))
+	{
+		cout << "\nYes, matrix is sparse." << endl;
+	}
+	else
+	{
+		cout << "\nNo, matrix is not sparse." << endl;
+	}
+}
